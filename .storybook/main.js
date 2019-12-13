2554576d7830c87dc266d6721593b67d4b7fe634
Trevor Eyre <trevoreyre@gmail.com>
2019-12-13 09:39:10 -0700

    Attempt to get MDX docs working

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,20 @@
+const util = require('util')
+
 module.exports = {
   presets: ['@storybook/addon-docs/preset'],
-  stories: ['../src/**/*.stories.(js|mdx)']
+  stories: ['../src/**/*.stories.(js|mdx)'],
+
+  // Modify webpack to remove babel-preset-vue from .mdx loaders
+  webpack: async config => {
+    const mdxRules = config.module.rules.filter(({ test }) => {
+      return String(test) === String(/\.mdx$/) || String(test) === String(/\.(stories|story).mdx$/)
+    })
+    mdxRules.forEach(mdxRule => {
+      const [babelLoader] = mdxRule.use.filter(({ loader }) => loader === 'babel-loader')
+      babelLoader.options.presets = babelLoader.options.presets.filter(preset => !preset.includes('babel-preset-vue'))
+    })
+
+    console.log(util.inspect(config.module.rules, false, null, true))
+    return config
+  }
 }
---
+++ src/components/HelloWorld.react.js
@@ -0,0 +1,3 @@
+import toReact from "@egoist/vue-to-react";
+import HelloWorld from "./HelloWorld.vue";
+export default toReact(HelloWorld);
