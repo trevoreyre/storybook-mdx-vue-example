053cc906269558270209ad4ec213715815e38b90
Lucas Teles <lucasvst@gmail.com>
2020-08-17 16:10:16 -0300

    updates storybook version: 5.x -> 6.x; (updates storybook config) (#6)

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,32 +6,33 @@ const filterRules = filters => rule => {
 
 module.exports = {
   presets: ["@storybook/addon-docs/preset"],
-  stories: ["../src/**/*.stories.(js|mdx)"],
+  stories: [
+    "../src/**/*.stories.js",
+    "../src/**/*.stories.mdx",
+  ],
 
   // Modify webpack to remove babel-preset-vue from .mdx loaders
-  webpack: async config => {
+  webpackFinal: async config => {
     const rules = config.module.rules;
-    const mdxRules = rules.filter(
-      filterRules([/\.mdx$/, /\.(stories|story).mdx$/])
-    );
-    let [jsxRule] = rules.filter(filterRules([/\.(mjs|jsx?)$/]));
 
+    const mdxRules = rules.filter(filterRules([/\.mdx$/, /\.(stories|story).mdx$/]))
     mdxRules.forEach(mdxRule => {
       const [babelLoader] = mdxRule.use.filter(
-        ({ loader }) => loader === "babel-loader"
-      );
+        ({ loader }) => loader.includes('babel-loader')
+      )
       babelLoader.options.presets = babelLoader.options.presets.filter(
         preset => !preset.includes("babel-preset-vue")
-      );
-    });
+      )
+    })
 
-    const [babelLoader] = jsxRule.use.filter(({ loader }) => loader === 'babel-loader')
+    const [jsxRule] = rules.filter(filterRules([/\.(mjs|jsx?)$/]))
+    const [babelLoader] = jsxRule.use.filter(({ loader }) => loader.includes('babel-loader'))
     babelLoader.options = {
       cacheDirectory: path.resolve(__dirname, '..', 'node_modules', '.cache', 'storybook'),
       presets: ['@vue/app'],
       babelrc: false
     }
 
-    return config;
+    return config
   }
 };
